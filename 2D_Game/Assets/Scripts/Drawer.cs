c7a8d32efa539173cc511464347cf570a43ede1f
Angelina <nguyenangelina192002@gmail.com>
2023-06-16 22:17:43 +0200

    Drawer
    - Drawer Open and close by colliding with VFT

---
+++ 2D_Game/Assets/Scripts/Drawer.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer : MonoBehaviour
+{
+    public GameObject Opened;
+    public GameObject Closed;
+    public GameObject Drawers;
+
+    public bool isOpen = false;
+
+    public void Start()
+    {
+        Drawers.transform.position = Closed.transform.position;
+        Drawers.GetComponent<BoxCollider2D>().enabled = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("VFT"))
+        {
+            if (!isOpen)
+            {
+                OpenDrawer();
+                Drawers.GetComponent<BoxCollider2D>().enabled = true;
+            }
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("VFT"))
+        {
+            if (isOpen)
+            {
+                CloseDrawer();
+                Drawers.GetComponent<BoxCollider2D>().enabled = false;
+
+            }
+        }
+    }
+    private void OpenDrawer()
+    {
+        Drawers.transform.position = Opened.transform.position;
+        isOpen = true;
+        Debug.Log("Drawer Open");
+    }
+
+    private void CloseDrawer()
+    {
+        Drawers.transform.position = Closed.transform.position;
+        isOpen = false;
+        Debug.Log("Drawer Closed");
+
+    }
+}
