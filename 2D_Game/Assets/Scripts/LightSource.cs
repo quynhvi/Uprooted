b70dea722df9062caef561cfc9bc68b99c66427f
Quynh Vi Trinh <quuynh.vi@gmail.com>
2023-06-26 15:59:42 +0200

    Organize Project
    - updated scripts to match codes

--- 2D_Game/Assets/Scripts/LightSource.cs
+++ 2D_Game/Assets/Scripts/LightSource.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 
 public class LightSource : MonoBehaviour
 {
-    private RessourceManagement _RessourceManagement;
-    public float _chargedLight;
+    private RessourceManagement ResourceManagement;
+    public float chargedLight;
     // Start is called before the first frame update
     void Awake()
     {
-        _RessourceManagement = GameObject.FindGameObjectWithTag("Game Manager").GetComponent<RessourceManagement>();
+        ResourceManagement = GameObject.FindGameObjectWithTag("Game Manager").GetComponent<RessourceManagement>();
     }
 
     // Update is called once per frame
@@ -21,16 +21,16 @@ public class LightSource : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collider) //Detecting if player characters pass through light and adding charged Light to the Level.
     {
-        _chargedLight = 0.0009f;
-        if (_RessourceManagement._lightLevelNumber < 1f && collider.gameObject.CompareTag("Cactus")) //Ivy erstmal rausgenommen! >>collider.gameObject.CompareTag("Ivy") || collider.gameObject.CompareTag("VFT") ||<<
+        chargedLight = 0.0009f;
+        if (ResourceManagement.lightLevelNumber < 1f && collider.gameObject.CompareTag("Cactus")) //Ivy erstmal rausgenommen! >>collider.gameObject.CompareTag("Ivy") || collider.gameObject.CompareTag("VFT") ||<<
         {
             Debug.Log("Player passed through light");
-            if (_RessourceManagement._lightLevelNumber > 1f)
+            if (ResourceManagement.lightLevelNumber > 1f)
                 return;
-            _RessourceManagement._lightLevelNumber += _chargedLight;
-            _RessourceManagement._lightBarFill.fillAmount += _chargedLight;
-            _RessourceManagement._waterLevelNumber -= _chargedLight;
-            _RessourceManagement._waterBarFill.fillAmount -= _chargedLight;
+            ResourceManagement.lightLevelNumber += chargedLight;
+            ResourceManagement.lightBarFill.fillAmount += chargedLight;
+            ResourceManagement.waterLevelNumber -= chargedLight;
+            ResourceManagement.waterBarFill.fillAmount -= chargedLight;
         }
     }
 }
