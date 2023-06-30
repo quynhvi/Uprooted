f9f4cf3bb04d8ec47290c4cd70792e19f29c3779
Quynh Vi Trinh <quuynh.vi@gmail.com>
2023-06-30 13:12:27 +0200

    Update GrabController.cs
    - fixed grab mechanic
    - will only grab when the right player is selected

--- 2D_Game/Assets/Scripts/GrabController.cs
+++ 2D_Game/Assets/Scripts/GrabController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -24,7 +26,8 @@ public class GrabController : MonoBehaviour
         interactable = true;
 
         gamepad = Gamepad.current;
-        playerSwap = GetComponent<PlayerSwap>(); // Get the PlayerSwap script reference from the player object
+        playerSwap = FindAnyObjectByType<PlayerSwap>().GetComponent<PlayerSwap>();
+        //playerSwap = GetComponent<PlayerSwap>(); // Get the PlayerSwap script reference from the player object
     }
 
     private void Update()
@@ -32,20 +35,8 @@ public class GrabController : MonoBehaviour
         droppedThisFrame = false;
 
         RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, Vector2.right * transform.localScale, rayDist);
-        GameObject objectToGrab = (grabCheck.collider != null && grabCheck.collider.CompareTag("dragable")) ? grabCheck.collider.gameObject : null;
 
         if (heldObject != null && playerSwap.whichCharacter == 1)
-        if ((Input.GetKeyDown(KeyCode.I) || (gamepad != null && gamepad.buttonWest.wasPressedThisFrame)) && heldObject != null)
-        {
-            Debug.Log("Object Released");
-
-            heldObject.transform.parent = null;
-            heldObject.GetComponent<Rigidbody2D>().isKinematic = false;
-            heldObject = null;
-            droppedThisFrame = true;
-        }
-
-        if (heldObject == null && objectToGrab != null && !droppedThisFrame)
         {
             // Release the object if it is held and the grab input is pressed
             if (Input.GetKeyDown(KeyCode.I) || (gamepad != null && gamepad.buttonWest.wasPressedThisFrame))
@@ -58,28 +49,27 @@ public class GrabController : MonoBehaviour
                 droppedThisFrame = true;
             }
         }
-        else if (heldObject == null && grabCheck.collider.CompareTag("dragable") && !droppedThisFrame && playerSwap.whichCharacter == 1)
+        if (grabCheck != false)
         {
-            Debug.Log("grabbed");
-            // Grab the object if it is interactable and the grab input is pressed, and the second character is the current one
-            if ((Input.GetKeyDown(KeyCode.I) || (gamepad != null && gamepad.buttonWest.wasPressedThisFrame)) && playerSwap.whichCharacter == 1)
+            if (heldObject == null && grabCheck.collider.CompareTag("dragable") && !droppedThisFrame && playerSwap.whichCharacter == 1)
             {
-                grabCheck.collider.gameObject.transform.parent = grabHolder;
-                grabCheck.collider.gameObject.transform.position = grabHolder.position;
-                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
-                heldObject = grabCheck.transform.gameObject;
-                objectToGrab.transform.parent = grabHolder;
-                objectToGrab.transform.position = grabHolder.position;
-                objectToGrab.GetComponent<Rigidbody2D>().isKinematic = true;
-                heldObject = objectToGrab;
-
-                // Decrease resource levels
-                if (rm != null && ls != null)
+                Debug.Log("grabbed");
+                // Grab the object if it is interactable and the grab input is pressed, and the second character is the current one
+                if ((Input.GetKeyDown(KeyCode.I) || (gamepad != null && gamepad.buttonWest.wasPressedThisFrame)) && playerSwap.whichCharacter == 1)
                 {
-                    rm.lightLevelNumber -= ls.chargedLight;
-                    rm.lightBarFill.fillAmount -= ls.chargedLight;
-                    rm.waterLevelNumber -= ls.chargedLight;
-                    rm.waterBarFill.fillAmount -= ls.chargedLight;
+                    grabCheck.collider.gameObject.transform.parent = grabHolder;
+                    grabCheck.collider.gameObject.transform.position = grabHolder.position;
+                    grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                    heldObject = grabCheck.transform.gameObject;
+
+                    // Decrease resource levels
+                    if (rm != null && ls != null)
+                    {
+                        rm.lightLevelNumber -= ls.chargedLight;
+                        rm.lightBarFill.fillAmount -= ls.chargedLight;
+                        rm.waterLevelNumber -= ls.chargedLight;
+                        rm.waterBarFill.fillAmount -= ls.chargedLight;
+                    }
                 }
             }
         }
