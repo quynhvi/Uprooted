c3e12b3a6b8dee5507d210a61bafa698d9218fce
Quynh Vi Trinh <quuynh.vi@gmail.com>
2023-07-10 17:21:43 +0200

    Add Journal

--- 2D_Game/Assets/Scripts/GMScript.cs
+++ 2D_Game/Assets/Scripts/GMScript.cs
@@ -5,23 +5,84 @@ using UnityEngine.InputSystem;
 
 public class GMScript : MonoBehaviour
 {
+    [SerializeField] private GameObject journalScreen;
+
     private Gamepad gamepad;
+    public InputActionReference journalAction;
+    public InputActionReference restartAction;
+
+    MenuInput action;
+    private bool paused;
+
+    private void Awake()
+    {
+        action = new MenuInput();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        gamepad = Gamepad.current;
+        //gamepad = Gamepad.current;
+        action.Menu.Journal.performed += _ => DeterminePause();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DeterminePause()
     {
-        if (Input.GetKeyDown(KeyCode.R) || gamepad != null && gamepad.selectButton.wasPressedThisFrame)
+        if (paused)
         {
-            // Reload the active scene
-            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+            CloseJournal();
+        }
+        else
+        {
+            OpenJournal();
         }
     }
+
+    private void OnEnable()
+    {
+        action.Enable();
+        
+
+        restartAction.action.Enable();
+        restartAction.action.performed += Restart;
+    }
+
+    private void OnDisable()
+    {
+        action.Disable();
+        
+    }
+
+    public void OpenJournal()
+    {
+        journalScreen.SetActive(true);
+        paused = true;
+        Time.timeScale = 0f;
+    }
+
+    public void CloseJournal()
+    {
+        Time.timeScale = 1f;
+        paused = false;
+        journalScreen.SetActive(false);   
+    }
+
+    public void Restart(InputAction.CallbackContext context)
+    {
+        Time.timeScale = 1f;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.R) || gamepad != null && gamepad.selectButton.wasPressedThisFrame)
+    //    {
+    //        // Reload the active scene
+    //        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+    //    }
+    //}
 
     public void GameOver()
     {
