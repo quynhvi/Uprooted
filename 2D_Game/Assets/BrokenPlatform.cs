cdbe02693bcd3e29aaeb54842e647000b0bf1594
Quynh Vi Trinh <quuynh.vi@gmail.com>
2023-07-15 16:50:24 +0200

    Broken Platform
    - broken platform to swinging object
    - adjusted collider

---
+++ 2D_Game/Assets/BrokenPlatform.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenPlatform : MonoBehaviour
+{
+    [SerializeField] private GameObject brokenPlatform;
+    [SerializeField] private GameObject halfBrokenPlatform;
+    [SerializeField] private GameObject platform;
+
+    private bool isBroken = false;
+    private int playerCount = 0;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Cactus") || other.gameObject.CompareTag("VFT") || other.gameObject.CompareTag("Ivy") || other.gameObject.CompareTag("AloeVera"))
+        {
+            playerCount++;
+        }
+
+        if (playerCount == 1 && !isBroken)
+        {
+            halfBrokenPlatform.SetActive(true);
+        }
+
+        if (playerCount >= 2 && !isBroken)
+        {
+            isBroken = true;
+            brokenPlatform.SetActive(true);
+            halfBrokenPlatform.SetActive(false);
+            platform.SetActive(false);
+        }
+        else if (playerCount < 2 && isBroken)
+        {
+            isBroken = false;
+            brokenPlatform.SetActive(false);
+            platform.SetActive(true);
+        }
+    }
+}
